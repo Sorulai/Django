e7b872677cebf3c06c29f93ef23b9d56710cee2b
Sorulai <57193937+Sorulai@users.noreply.github.com>
2021-10-28 20:06:09 +0300

    init

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('products/', include('mainapp.urls', namespace='products')),
     path('basket/',include('basketapp.urls',namespace='basket')),
     path('auth/', include('authapp.urls', namespace='auth')),
-    path('basket/', include('basketapp.urls', namespace='basket')),
     path('admin/', admin.site.urls),
 ]
 
