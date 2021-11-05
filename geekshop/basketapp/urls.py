b250676c351a90d28be36eec5e4f5f4b350ae966
Sorulai <57193937+Sorulai@users.noreply.github.com>
2021-11-05 13:42:35 +0300

    lesson6

--- geekshop/basketapp/urls.py
+++ geekshop/basketapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.basket, name='basket'),
     path('add/<int:pk>/', views.add, name='add'),
     path('remove/<int:pk>/', views.remove, name='remove'),
-    path('edit/<int:pk>/<int:quantity>', views.edit, name='edit'),
+    path('edit/<int:pk>/<int:quantity>/', views.edit, name='edit'),
 ]
