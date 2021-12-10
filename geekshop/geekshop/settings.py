f809e7b95ddc584e80a49a042f3a4dbb88b9e1a3
Sorulai <57193937+Sorulai@users.noreply.github.com>
2021-12-10 19:30:26 +0300

    dev_6

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -214,3 +214,15 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+if ENV_TYPE == 'dev':
+    CACHE_MIDDLEWARE_ALIAS = 'default'
+    CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+    CACHE_MIDDLEWARE_SECONDS = 120
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211'
+        }
+    }
+    LOW_CACHE = True
